fefacce056530509be764c01f0ff8041b28e9bd4
Andy Arias <146486695+andyarias30@users.noreply.github.com>
2023-11-24 12:58:49 -0500

    sugarLevel Api

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from 'express'
+import cors from 'cors'
+import { getAllSugarLevels, addSugarLevel } from './src/sugarLevels.js'
+
+const PORT = process.env.PORT || 3005
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+app.get("/mySugarLevels", getAllSugarLevels)
+app.post("/mysugarLevels", addSugarLevel)
+
+app.listen(PORT, () => {
+    console.log(` Listening on http://localhost:${PORT}...`)
+})
---
+++ src/sugarLevels.js
@@ -0,0 +1,22 @@
+import { db } from './connectDb.js'
+
+export function getAllSugarLevels(req, res) {
+    db.collection('sugarLevels').find({}).toArray()
+        .then(sugarLevels => {
+            res.send(sugarLevels)
+        })
+        .catch(err => {
+            res.status(500).send({ success: false, message: err })
+        })
+}
+
+export function addSugarLevel(req, res) {
+    const newSugarLevel = req.body
+    db.collection('sugarLevels').insertOne(newSugarLevel)
+        .then(() => {
+            res.status(201).send({ message: "Sugar Level added", success: true })
+        })
+        .catch(err => {
+            res.status(500).send({ success: false, message: err })
+        })
+}
