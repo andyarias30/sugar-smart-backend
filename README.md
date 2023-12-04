2ca097ffb8e0977c03190b72a60fb73db1f0d404
Andy Arias <146486695+andyarias30@users.noreply.github.com>
2023-12-04 15:34:57 -0500

    adding new functions and filters

---
+++ README.md
@@ -0,0 +1,19 @@
+# Portfolio c12
+
+[![thubnali](./images/car.jpeg)
+](https://bocacode.com)
+
+## Website
+[Check out my site at Bocacode.com](https://bocacode.com)
+
+## Note
+* 1
+* 2
+
+## Resource
+*[sometething.com](link here)
+
+## Additional
+*Error Message
+> Something here 
+
--- src/sugarLevels.js
+++ src/sugarLevels.js
@@ -10,14 +10,28 @@ export function getAllSugarLevels(req, res) {
         })
 }
 
-export function addSugarLevel(req, res) {
-    const newSugarLevel = req.body
-    db.collection('sugarLevels').insertOne(newSugarLevel)
-        .then(() => {
-            res.status(201).send({ message: "Sugar Level added", success: true })
-        })
-        .catch(err => {
-            res.status(500).send({ success: false, message: err })
-        })
+export async function addSugarLevel(req, res) {
+    console.log("req.doy-->", req.body)
+    const currentDate = new Date()
+    currentDate.setUTCHours(0, 0, 0, 0)
+    const userData = await db.collection('sugarLevels').find({ userName: req.body.userName, currentDate: { $gte: currentDate } }).toArray() // filter to find the objects of the same user whit the same date 
+    console.log('userData-->', userData)
+    console.log('currentDate ->', currentDate)
+
+    if (userData.length > 0) {
+       await db.collection('sugarLevels').findOneAndUpdate({ userName: req.body.userName, currentDate: { $gte: currentDate } }, { $set: req.body }) 
+       res.status(201).send({message: "sugar level found it", success: true})
+        console.log('next steps')
+    } else {
+        const newSugarLevel = req.body
+        db.collection('sugarLevels').insertOne({ ...newSugarLevel, currentDate: currentDate })
+            .then(() => {
+                res.status(201).send({ message: "Sugar Level added", success: true })
+            })
+            .catch(err => {
+                res.status(500).send({ success: false, message: err })
+            })
+    }
 }
+
 
